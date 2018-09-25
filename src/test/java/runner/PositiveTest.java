ad4080c8b7348211597af3f7959bc4b123684de6
amisharm25 <amisharm25@WKWIN2530586.global.publicisgroupe.net>
2018-09-25 15:48:57 +0530

    comit

--- src/test/java/runner/PositiveTest.java
+++ src/test/java/runner/PositiveTest.java
@@ -2,13 +2,18 @@ package runner;
 
 import java.io.File;
 
+
 import org.testng.annotations.AfterClass;
 
 import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
-
+/**
+ * 
+ * @author amisharm25
+ *
+ */
 @CucumberOptions(plugin = {"json:target/positive/cucumber.json","pretty","com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, features = "src/test/resources/FeatureFile/SearchDress.feature", glue = "steps", monochrome = true, strict = true)
 public class PositiveTest extends AbstractTestNGCucumberTests {
 	@AfterClass
