777eba52e8ae62110d92ad69bf9eccd61f1128d2
amisharm25 <amisharm25@WKWIN2530586.global.publicisgroupe.net>
2018-09-25 15:23:24 +0530

    third push

--- src/main/java/util/SeleniumDriver.java
+++ src/main/java/util/SeleniumDriver.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumDriver {
-//changes saved ok
+//changes saved ok again ok Then
 	private static SeleniumDriver seleniumDriver;
 
 	// initialize webdriver
