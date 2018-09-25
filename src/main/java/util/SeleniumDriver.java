84ed3eda6d7cf61a1b3cc17d880a16a9e04ecd0c
amisharm25 <amisharm25@WKWIN2530586.global.publicisgroupe.net>
2018-09-25 14:37:56 +0530

    commiting

--- src/main/java/util/SeleniumDriver.java
+++ src/main/java/util/SeleniumDriver.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumDriver {
-
+//changes saved
 	private static SeleniumDriver seleniumDriver;
 
 	// initialize webdriver
